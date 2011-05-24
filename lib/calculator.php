41b4fbeacb765f977e3372f98e076850d4b423b8
Jamie Rumbelow <jamie@jamierumbelow.net>
2011-05-24 21:13:41 +0100

    Working example implementation

--- lib/calculator.php
+++ lib/calculator.php
@@ -1,7 +1,18 @@
 <?php
+/**
+ * Inferno
+ *
+ * Quick, lightweight and simple to use 
+ * PHP unit testing library
+ *
+ * @author Jamie Rumbelow <http://jamierumbelow.net>
+ * @version 0.1.0
+ * @copyright Copyright (c)2011 Jamie Rumbelow
+ * @package default
+ **/
 
 class Calculator {
-	public static function add() {
-		
+	public static function add($one, $two) {
+		return $one + $two;
 	}
 }
